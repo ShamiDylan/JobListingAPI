4a1b3d7925ee080fce743580ef155879f05949c1
unknown <dylan@techclick.rw>
2024-08-30 10:31:26 +0200

    adding the database

--- src/main/java/com/example/job/listings/JobListingsApplication.java
+++ src/main/java/com/example/job/listings/JobListingsApplication.java
@@ -2,13 +2,19 @@ package com.example.job.listings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 
 
 @SpringBootApplication
 public class JobListingsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// Set environment variables for Spring Boot
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
 		SpringApplication.run(JobListingsApplication.class, args);
 	}
 
